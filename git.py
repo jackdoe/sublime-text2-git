041a2129b38c4a47ad28da9eef7fcc3bf97ee913
borislav nikolov <jack@brokensociety.org>
2012-09-12 22:23:59 +0300

    aand add threading again

--- git.py
+++ git.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, subprocess, os
+import sublime, sublime_plugin, subprocess, thread, os, functools, glob, fnmatch
 class PromptGitCommitCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window.active_view().run_command('save')
@@ -13,27 +13,35 @@ class PromptGitCommitCommand(sublime_plugin.WindowCommand):
             pass
 
 class GitCommand(sublime_plugin.TextCommand): 
-    def run_command(self,edit,command): 
+    def run_thread(self,edit,command):
         os.chdir(self.folder())
-        proc = subprocess.Popen(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=False)
+        thread.start_new_thread(self.run_command,(edit,command))
+    def run_command(self,edit,command): 
+        proc = subprocess.Popen(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE,shell=False, universal_newlines=True)
         self.output = proc.communicate()[0].split("\n")
-        self.view.window().show_quick_panel(self.output,self.panel_done,sublime.MONOSPACE_FONT)
+        sublime.set_timeout(self.draw, 20)
+    def win(self):
+        return self.view.window() or sublime.active_window()
+    def draw(self):
+        self.win().show_quick_panel(self.output,self.panel_done,sublime.MONOSPACE_FONT)
     def panel_done(self, index):
         pass
     def folder(self):
-        return self.view.window().folders().pop()
+        return self.win().folders().pop()
 
 class GitPullCommand(GitCommand):
     def run(self,edit):
-        self.run_command(edit,["git","pull"])
+        self.run_thread(edit,["git","pull"])
+
 class GitPushCommand(GitCommand):
     def run(self,edit):
-        self.run_command(edit,["git","push","origin","master"])
+        self.run_thread(edit,["git","push","-u","origin","master"])
+
 class GitLogCommand(GitCommand):
     def run(self,edit):
-        self.run_command(edit,["git","log",'--pretty=format:%s [%ce %h, %ar]',"--abbrev-commit"])
+        self.run_thread(edit,["git","log",'--pretty=format:%s [%ce %h, %ar]',"--abbrev-commit"])
 
 class GitCommitCommand(GitCommand):
     def run(self,edit,line):
-        self.run_command(edit,["git","commit","-a","-m",line])
+        self.run_thread(edit,["git","commit","-a","-m",line])
 
